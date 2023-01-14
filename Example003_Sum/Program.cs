bb17d15d5138f3b82fe7e5f83183e511ae52b86b
Nataly <nt.kuzovleva@gmail.com>
2023-01-15 02:04:01 +0300

    Example003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿// Написать программу на языке C#,
+// для Windows и Mac OS,
+// [в которую пользователь вводит два целых числа]
+// [в которой записаны два целых числа]
+// [два целых числа генерируются случайным образом]
+// и получает сумму
+
+// Вариант 1
+// int numberA = 3;
+// int numberB = 5;
+// Console.WriteLine(numberA + numberB);
+
+// Вариант 2
+int numberA = 31;
+int numberB = 58;
+int result = numberA + numberB;
+Console.WriteLine(result);
+
+
+// Мой вариант
+// Console.WriteLine("Введите первое число:");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// // Console.WriteLine($"Сумма двух чисел равна {number1 + number2}");
+// Console.WriteLine($"Сумма чисел {number1} и {number2} равна {number1 + number2}");
